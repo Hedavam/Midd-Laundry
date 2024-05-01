80d19d76c2934b625d202973a51eec49ccba8f17
Vikram Vasan <vvasan@middlebury.edu>
2024-05-01 14:12:49 -0400

    small testid change

--- src/pages/[room].js
+++ src/pages/[room].js
@@ -185,7 +185,6 @@ export default function Room({ currentRoom, setCurrentRoom }) {
               .sort((a, b) => a.MachineNum - b.MachineNum)
               .map((dryer) => (
                 <Machine
-                  data-testid="dryer"
                   key={dryer.id}
                   id={dryer.id}
                   MachineNum={dryer.MachineNum}
