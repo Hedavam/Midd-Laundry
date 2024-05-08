3c9a75af7d0894329f77d8f0d0110e11898dc01e
Vikram Vasan <vvasan@middlebury.edu>
2024-05-08 14:56:28 -0400

    uncommented loads fetch

--- src/pages/api/loads/[id].js
+++ src/pages/api/loads/[id].js
@@ -4,35 +4,35 @@
   If an user messes up load info, 
   just finish and start new load */
 
-// import { createRouter } from "next-connect";
-// import { onError } from "../../../lib/middleware";
-// import Loads from "../../../../models/Loads";
+import { createRouter } from "next-connect";
+import { onError } from "../../../lib/middleware";
+import Loads from "../../../../models/Loads";
 
-// const router = createRouter();
+const router = createRouter();
 
-// /* PUT method -> will update a load's info in case user finished load before duration + buffer or they mess up! */
-// // router.put(async (req, res) => {
-// //   const { id, ...updatedLoad } = req.body;
-// //   // req.query.id is a string, and so needs to be converted to an integer before comparison
-// //   if (id !== parseInt(req.query.id, 10)) {
-// //     // req.query.id is what we feeding from the api route!
-// //     // Verify id in the url, e.g, /api/loads/1, matches the id the request body; hmm, in front-end we won't rlly have notion of loads???
-// //     res.status(400).end(`URL and object does not match`);
-// //     return;
-// //   }
-// //   // Update Database
-// //   const load = await Loads.query()
-// //     .where("MachineId", req.body.MachineId) // the body is the object we're feeding in, so we extract machineId
-// //     .findOne("End", ">", new Date().toISOString());
+/* PUT method -> will update a load's info in case user finished load before duration + buffer or they mess up! */
+router.put(async (req, res) => {
+  const { id, ...updatedLoad } = req.body;
+  // req.query.id is a string, and so needs to be converted to an integer before comparison
+  if (id !== parseInt(req.query.id, 10)) {
+    // req.query.id is what we feeding from the api route!
+    // Verify id in the url, e.g, /api/loads/1, matches the id the request body; hmm, in front-end we won't rlly have notion of loads???
+    res.status(400).end(`URL and object does not match`);
+    return;
+  }
+  // Update Database
+  const load = await Loads.query()
+    .where("MachineId", req.body.MachineId) // the body is the object we're feeding in, so we extract machineId
+    .findOne("End", ">", new Date().toISOString());
 
-// //   if (load) {
-// //     const latestLoad = await load.$query().updateAndFetch(updatedLoad);
-// //     res.status(200).json(latestLoad);
-// //   } else {
-// //     res.status(200).json({});
-// //   }
-// // });
+  if (load) {
+    const latestLoad = await load.$query().updateAndFetch(updatedLoad);
+    res.status(200).json(latestLoad);
+  } else {
+    res.status(200).json({});
+  }
+});
 
-// // Notice the `onError` middleware for aspect-oriented error handler. That middleware
-// // will be invoked if the handler code throws an exception.
-// export default router.handler({ onError });
+// Notice the `onError` middleware for aspect-oriented error handler. That middleware
+// will be invoked if the handler code throws an exception.
+export default router.handler({ onError });
